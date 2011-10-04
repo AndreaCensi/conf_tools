8e1927a92b933e2fdc2ec7f1e827dd2114840ad9
Andrea Censi <andrea@cds.caltech.edu>
2011-10-03 22:03:09 -0700

    /

--- src/conf_tools/master.py
+++ src/conf_tools/master.py
@@ -1,20 +1,29 @@
 from . import load_configuration_entries, logger
 from UserDict import IterableUserDict
 from abc import abstractmethod
-
+from contracts import contract
 
 class ObjectSpec(IterableUserDict):
-    def __init__(self, name, pattern, check, instance_method):
+    def __init__(self, name, pattern, check, instance_method, master):
         self.name = name
         self.pattern = pattern
         self.check = check
         self.instance_method = instance_method
+        self.master = master
         IterableUserDict.__init__(self)
 
+    @contract(id_object='str')
     def instance(self, id_object):
+        self.master.make_sure_loaded()
+        if not id_object in self.data:
+            msg = ('No %s %r known; I know %s'  
+                    % (self.name, id_object, self.data.keys()))
+            raise ValueError(msg)
         return self.instance_spec(self.data[id_object])
-      
+    
+    @contract(spec='dict')
     def instance_spec(self, spec):
+        self.master.make_sure_loaded()
         if self.instance_method is None:
             msg = 'No instance method specified for %r' % self.name
             raise ValueError(msg)
@@ -28,12 +37,17 @@ class ConfigMaster:
         self.specs = {}
         
     def add_class(self, name, pattern, check=None, instance=None):
-        self.specs[name] = ObjectSpec(name, pattern, check, instance)
+        self.specs[name] = ObjectSpec(name, pattern, check, instance, self)
         
     @abstractmethod
     def get_default_dir(self):
         pass
     
+    def make_sure_loaded(self):
+        ''' If the configuration is not been loaded yet, load the default one. '''
+        if not self.loaded:
+            self.load(None)
+        
     def load(self, directory=None):
         if directory is None:
             directory = self.get_default_dir()
