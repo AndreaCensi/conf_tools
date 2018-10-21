e9c3879b929c9c16eeee9fccc9d7f2f050c375de
Andrea Censi <acensi@ethz.ch>
2018-10-21 16:38:28 +0200

    fix

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 import os
+
 from setuptools import setup, find_packages
 
 
 def get_version(filename):
     import ast
     version = None
-    with file(filename) as f:
+    with open(filename) as f:
         for line in f:
             if line.startswith('__version__'):
                 version = ast.parse(line).body[0].value.s
@@ -16,41 +17,41 @@ def get_version(filename):
         raise ValueError(filename)
     return version
 
-version = get_version(filename='src/conf_tools/__init__.py')
 
+version = get_version(filename='src/conf_tools/__init__.py')
 
 description = """"""
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
-    
+
+
 long_description = read('README.md')
-    
 
 setup(name='ConfTools',
       author="Andrea Censi",
       author_email="censi@mit.edu",
       url='http://github.com/AndreaCensi/conf_tools/',
-      
+
       description=description,
       long_description=long_description,
       keywords="configuration",
       license="LGPL",
-      
+
       classifiers=[
-        'Development Status :: 4 - Beta',
+          'Development Status :: 4 - Beta',
       ],
 
-	  version=version,
+      version=version,
       download_url='http://github.com/AndreaCensi/conf_tools/tarball/%s' % version,
-      
-      package_dir={'':'src'},
+
+      package_dir={'': 'src'},
       packages=find_packages('src'),
-      install_requires=[ 
-        'PyYAML', 
-        'PyContracts>=1.2,<2',
+      install_requires=[
+          'PyYAML',
+          'PyContracts>=1.2,<2',
       ],
       tests_require=['nose'],
       entry_points={},
-)
-
+      )
