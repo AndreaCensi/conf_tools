d86d8150afe8fe08de5decfd9d100f422eeb4af4
Andrea Censi <acensi@ethz.ch>
2022-01-21 19:39:18 +0100

    try to fix
    [ci skip]

--- src/conf_tools/utils/term_color.py
+++ src/conf_tools/utils/term_color.py
@@ -10,7 +10,6 @@ try:
     def termcolor_colored(s, color=None, on_color=None, attrs=None):
         return "\n".join(t_colored(x, color, on_color, attrs) for x in s.split("\n"))
 
-
 except:
     # TODO: logger
     sys.stderr.write('compmake can make use of the package "termcolor".' " Please install it.\n")
