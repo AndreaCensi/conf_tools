b0f1516938d8e2dcd3afe58345b8edf9677a6fd4
Andrea Censi <acensi@ethz.ch>
2022-04-19 16:36:21 +0200

    [ci skip] split vs splitlines

--- src/conf_tools/utils/term_color.py
+++ src/conf_tools/utils/term_color.py
@@ -4,20 +4,24 @@ __all__ = [
     "termcolor_colored",
 ]
 
+from typing import List, Optional
+
 from zuper_commons.text import joinlines
 
 try:
     from termcolor import colored as t_colored
 
-
-    def termcolor_colored(s, color=None, on_color=None, attrs=None):
+    def termcolor_colored(
+        s: str, color: Optional[str] = None, on_color: Optional[str] = None, attrs: Optional[List[str]] = None
+    ) -> str:
         return joinlines(t_colored(x, color, on_color, attrs) for x in s.splitlines())
 
 except:
     # TODO: logger
     sys.stderr.write('compmake can make use of the package "termcolor".' " Please install it.\n")
 
-
-    def termcolor_colored(x, color=None, on_color=None, attrs=None):  # @UnusedVariable
+    def termcolor_colored(
+        s: str, color: Optional[str] = None, on_color: Optional[str] = None, attrs: Optional[List[str]] = None
+    ) -> str:
         """emulation of the termcolor interface"""
-        return x
+        return s
