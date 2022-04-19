f189102689267003777179db0ec27f5051e9d80b
Andrea Censi <acensi@ethz.ch>
2022-04-19 15:04:39 +0200

    templates [ci skip] DEV-2

--- src/conf_tools/utils/term_color.py
+++ src/conf_tools/utils/term_color.py
@@ -9,12 +9,14 @@ from zuper_commons.text import joinlines
 try:
     from termcolor import colored as t_colored
 
+
     def termcolor_colored(s, color=None, on_color=None, attrs=None):
         return joinlines(t_colored(x, color, on_color, attrs) for x in s.splitlines())
 
 except:
     # TODO: logger
     sys.stderr.write('compmake can make use of the package "termcolor".' " Please install it.\n")
+
 
     def termcolor_colored(x, color=None, on_color=None, attrs=None):  # @UnusedVariable
         """emulation of the termcolor interface"""
