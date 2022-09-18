47ac1f92d5c8403de9807313819835466523127a
Andrea Censi <acensi@ethz.ch>
2022-09-18 15:21:22 +0200

    DEV-119 Do direct REST API commits on Github [ci skip]

--- src/conf_tools/utils/term_color.py
+++ src/conf_tools/utils/term_color.py
@@ -1,27 +1,17 @@
-import sys
+from typing import Optional, Sequence
+
+from zuper_commons.ui import get_colorize_function
 
 __all__ = [
     "termcolor_colored",
 ]
 
-from typing import List, Optional
 
-from zuper_commons.text import joinlines
-
-try:
-    from termcolor import colored as t_colored
-
-    def termcolor_colored(
-        s: str, color: Optional[str] = None, on_color: Optional[str] = None, attrs: Optional[List[str]] = None
-    ) -> str:
-        return joinlines(t_colored(x, color, on_color, attrs) for x in s.splitlines())
-
-except:
-    # TODO: logger
-    sys.stderr.write('compmake can make use of the package "termcolor".' " Please install it.\n")
-
-    def termcolor_colored(
-        s: str, color: Optional[str] = None, on_color: Optional[str] = None, attrs: Optional[List[str]] = None
-    ) -> str:
-        """emulation of the termcolor interface"""
-        return s
+def termcolor_colored(x, color: str, on_color: Optional[str] = None, attrs: Optional[Sequence[str]] = None):
+    cf = get_colorize_function(color, on_color, attrs)
+    colorize = True
+    # TODO: no colorize during tests
+    if colorize:  # @UndefinedVariable
+        return cf(x)
+    else:
+        return x
